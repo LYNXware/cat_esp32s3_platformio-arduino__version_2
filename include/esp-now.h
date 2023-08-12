366d84ee084a8fdbb2c2bd7f5b2c9ed102c20a9a
LYNXware <97060051+LYNXware@users.noreply.github.com>
2023-08-12 16:37:15 +0200

    first stepes of espNow

--- include/esp-now.h
+++ include/esp-now.h
@@ -4,15 +4,33 @@
 #include <Arduino.h>
 
 
+#include <WiFi.h>
+#include <esp_now.h>
 
 
 class EspNow {
 
     public:
 
+        void initialize();
+
+        // void send_data(uint8_t data[]);
+
+        // void receive_data(uint8_t data[]);
+
+
+    private:
+    
+        static void OnDataSent(const uint8_t* mac_addr, esp_now_send_status_t status);
+
+        static void OnDataReceived(const uint8_t* mac_addr, const uint8_t* data, int data_len); 
+
+
+
+
 };
 
-
+//instance of the class 
 extern EspNow espnow;
 
 #endif
--- src/esp-now.cpp
+++ src/esp-now.cpp
@@ -1,1 +1,38 @@
 #include "esp-now.h"
+
+//instance of the class
+EspNow espnow;
+
+void EspNow::initialize() {
+
+
+    uint8_t mac[6];
+    esp_read_mac(mac, ESP_MAC_WIFI_STA);
+
+
+    // Initialize ESP-NOW
+    WiFi.mode(WIFI_STA);
+    esp_now_init();
+
+    // Add peer
+    esp_now_peer_info_t peerInfo;
+    memcpy(peerInfo.peer_addr, mac, 6);
+    peerInfo.channel = 0;
+    peerInfo.encrypt = false;
+    esp_now_add_peer(&peerInfo);
+
+    // Register callback functions
+    esp_now_register_send_cb(OnDataSent);
+    esp_now_register_recv_cb(OnDataReceived);
+   
+}
+
+// Callback function for sending data
+void OnDataSent(const uint8_t* mac_addr, esp_now_send_status_t status) {
+  // Handle send status
+}
+
+// Callback function for receiving data
+void OnDataReceived(const uint8_t* mac_addr, const uint8_t* data, int data_len) {
+  // Handle received data
+}
